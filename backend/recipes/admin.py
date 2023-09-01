97827ccc617ac2b3985fdce80d7940d376a2b4b8
JRushFobos <fobos_media@mail.ru>
2023-09-01 10:50:23 +0500

    add IngredientsInLine in recipes admin  v.1.0.29

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -5,37 +5,67 @@ from .models import FavouriteRecipe, Ingredient, Recipe, ShoppingList, Tag
 
 @admin.register(Tag)
 class TagInAdmin(admin.ModelAdmin):
-    """Модель тегов для отображения в админке"""
+    """Класс тегов для отображения в админке."""
 
+    empty_value_display = "значение отсутствует"
     list_display = ('id', 'name', 'color', 'slug')
     search_fields = ('name',)
 
 
 @admin.register(Ingredient)
 class IngredientInAdmin(admin.ModelAdmin):
-    """Модель ингредиентов для отображения в админке"""
+    """Класс ингредиентов для отображения в админке."""
 
+    empty_value_display = "значение отсутствует"
     list_display = ('id', 'name', 'measurement_unit')
     search_fields = ('name',)
 
 
+class IngredientsInLine(admin.TabularInline):
+    model = Recipe.ingredients.through
+    extra = 0
+    mix_num = 1
+
+
 @admin.register(Recipe)
 class RecipeInAdmin(admin.ModelAdmin):
-    """Модель рецептов для отображения в админке"""
+    """Класс рецептов для отображения в админке."""
 
-    list_display = ("id", "name", "author")
+    empty_value_display = "значение отсутствует"
+    list_display = (
+        "id",
+        "name",
+        "author",
+        "text",
+        "get_ingredients",
+        "cooking_time",
+        "image",
+        "pub_date",
+        "count_favourites",
+    )
     list_filter = ("name", "author", "tags")
+    inlines = (IngredientsInLine,)
     search_fields = ("name", "author")
-    readonly_fields = ("count_favorites",)
 
-    def count_favorites(self, obj):
-        return obj.favorites.count()
+    def get_ingredients(self, object):
+        """Метод получения ингредиентов в рецепте в админке."""
+        return "\n".join(
+            ingredient.name for ingredient in object.ingredients.all()
+        )
+
+    get_ingredients.short_description = "Ингредиенты"
+
+    def count_favourites(self, obj):
+        return obj.favourites.count()
+
+    count_favourites.short_description = "Количество добавлений в избранное"
 
 
 @admin.register(FavouriteRecipe)
 class FavouriteRecipeInAdmin(admin.ModelAdmin):
-    """Модель избранных для отображения в админке"""
+    """Класс избранных для отображения в админке."""
 
+    empty_value_display = "значение отсутствует"
     list_display = ("id", "user", "recipe")
     search_fields = ("user", "recipe")
     list_filter = ("user", "recipe")
@@ -43,8 +73,9 @@ class FavouriteRecipeInAdmin(admin.ModelAdmin):
 
 @admin.register(ShoppingList)
 class ShoppingListInAdmin(admin.ModelAdmin):
-    """Модель избранных для отображения в админке"""
+    """Класс избранных для отображения в админке."""
 
+    empty_value_display = "значение отсутствует"
     list_display = ("id", "user", "recipe")
     search_fields = ("user", "recipe")
     list_filter = ("user", "recipe")
