01f44d92f91cd065ed6c58af41d45d459be74095
JRushFobos <fobos_media@mail.ru>
2024-02-07 20:52:32 +0500

    deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -177,8 +177,11 @@ TITLE_SHOP_LIST = "Список покупок с сайта Foodgram:\n\n"
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "collected_static"
 
-MEDIA_URL = f"{os.getenv('ALLOWED_HOSTS')}/media/"
-STATIC_URL = f"{os.getenv('ALLOWED_HOSTS')}/static/"
+# MEDIA_URL = f"{os.getenv('ALLOWED_HOSTS')}/media/"
+# STATIC_URL = f"{os.getenv('ALLOWED_HOSTS')}/static/"
+
+MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
