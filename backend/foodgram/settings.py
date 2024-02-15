e8146fe3ecfb09cfa6e6f6ba4890b0fc64589384
JRushFobos <fobos_media@mail.ru>
2024-02-15 19:17:34 +0500

    deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -187,3 +187,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SECURE_SSL_REDIRECT = True
