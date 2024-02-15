467f38f0c73ba5124ad3e6914ff391186d0cec39
JRushFobos <fobos_media@mail.ru>
2024-02-15 23:00:16 +0500

    images bugfix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -187,5 +187,3 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
